31b74392e5f6addeba09bea265f6bba56da5d7fa
Nikita <87658743+bluntvicky@users.noreply.github.com>
2021-10-28 17:16:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const swiper = new Swiper('.swiper', {
+  speed: 900,
+  resistanceRatio: 1.6,
   // Optional parameters
   direction: 'horizontal',
   loop: true,
   pagination: {
     el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 5000,
   },
   // Navigation arrows
   navigation: {
